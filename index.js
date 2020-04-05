b5df4102f0c07ad4a76bc2ac52c47af45b7b5021
Jisoon <siriusda2@naver.com>
2020-04-05 21:05:35 +0900

    DB情報設定管理

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
 const express = require('express')
 const app = express()
 const port = 5000
+const bodyParser = require('body-parser');
+
+const config = require('./config/key');
+
+const { User } = require("./models/User");
+
+//application/x-form-urlencoded
+app.use(bodyParser.urlencoded({extended: true}));
+//application/json
+app.use(bodyParser.json());
 
 const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://<user>:<password>@board-vpfyk.mongodb.net/test?retryWrites=true&w=majority', {
+mongoose.connect(config.mongoURI, {
     useNewUrlParser: true, 
     useUnifiedTopology: true, 
     useCreateIndex: true, 
@@ -12,4 +22,15 @@ mongoose.connect('mongodb+srv://<user>:<password>@board-vpfyk.mongodb.net/test?r
   .catch(err => console.log(err))
   
 app.get('/', (req, res) => res.send('Hello World!'))
+
+app.post('/register', (req, res) => {
+    const user = new User(req.body)
+    user.save((err, doc) => {
+        if(err) return res.json({ success: false, err })
+        return res.status(200).json({
+            success: true
+        })
+    })
+})
+
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
