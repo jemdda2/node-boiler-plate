d567838fa1b03271d44ffc416a7116ad4fca12d6
Jisoon <siriusda2@naver.com>
2020-04-08 12:22:44 +0900

    Loing画面追加

---
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -0,0 +1,21 @@
+import React, { useEffect} from 'react'
+import axios from 'axios';
+
+function LandingPage() {
+
+    useEffect(() => {
+        axios.get('/api/hello')
+        .then(response => console.log(response.data))
+    }, [])
+
+    return (
+        <div style={{
+            display: 'flex', justifyContent: 'center', alignItems: 'center',
+            width: '100%', height: '100vh'
+        }}>
+            <h2>スタート画面</h2>
+        </div>
+    )
+}
+
+export default LandingPage
--- server/index.js
+++ server/index.js
@@ -1,6 +1,5 @@
 const express = require('express')
 const app = express()
-const port = 5000
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const config = require('./config/key');
@@ -23,6 +22,11 @@ mongoose.connect(config.mongoURI, {
   .catch(err => console.log(err))
   
 app.get('/', (req, res) => res.send('Hello World!'))
+
+app.get('/api/hello', (req, res) => {
+    res.send("こんにちは！");
+})
+
 
 app.post('/api/users/register', (req, res) => {
     
@@ -90,5 +94,7 @@ app.get('/api/users/logout', auth, (req, res) => {
             });
         });
 })
+
+const port = 5000
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
